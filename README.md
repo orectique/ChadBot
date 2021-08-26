a92dcaacbaa614f0fd0d61275f8403eae3423761
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2021-08-26 06:13:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## ChadBot
+
+A bot to spam your servers with the phrase 'na he tweaking'. ChadBot also changes the nicknames of people with no 'manage messages' permission to 'Chad'.
---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.author.guild_permissions.manage_messages == False:
+        await message.author.edit(nick = 'Chad')
+    await message.channel.send('na he tweaking')
+      
+    
+TOKEN = os.getenv("TOKEN")
+client.run(TOKEN)
